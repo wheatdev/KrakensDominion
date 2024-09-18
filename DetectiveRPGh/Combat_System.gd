6862e500dcd5bb5e9d1c8a369136bd88bbe5182a
Andrew Gearing <andrew.gearing1@marist.edu>
2024-09-18 15:42:28 -0400

    Combat System Prototype

---
+++ DetectiveRPGh/Combat_System.gd
@@ -0,0 +1,13 @@
+extends Container
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#TODO Implement all the fish booleans for potential attacks to enable and disable
---
+++ DetectiveRPGh/Inventory_Button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+signal show_inventory
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	show_inventory.emit()
